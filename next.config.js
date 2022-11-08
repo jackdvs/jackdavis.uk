9841db0e5057dc6ff0044bcb832a57a9d0ccdb19
jackdvs <j4ckdavis@gmail.com>
2022-11-08 16:48:19 +0000

    Removed redirects

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false,
   reactStrictMode: true,
   swcMinify: true,
   redirects: async () => [
     {
       source: "/:path*",
-      has: [
-        { type: "host", value: "wwww.jackdavis.me" },
-        { type: "host", value: "wwww.jaack.co.uk" },
-        { type: "host", value: "jaack.co.uk" },
-      ],
+      has: [{ type: "host", value: "wwww.jackdavis.me" }],
       destination: "https://jackdavis.me/:path*",
       permanent: false,
     },
