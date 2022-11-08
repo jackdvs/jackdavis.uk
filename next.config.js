f63245cd4c2ccb5ea24be865c76b990bba14f29c
jackdvs <j4ckdavis@gmail.com>
2022-11-08 01:33:28 +0000

    First commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  redirects: async () => [
+    {
+      source: "/:path*",
+      has: [
+        { type: "host", value: "wwww.jackdavis.me" },
+        { type: "host", value: "wwww.jaack.co.uk" },
+        { type: "host", value: "jaack.co.uk" },
+      ],
+      destination: "https://jackdavis.me/:path*",
+      permanent: false,
+    },
+  ],
+};
+
+module.exports = nextConfig;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="flex justify-center items-center flex-col gap-3 p-4 min-h-[300px] sm:min-h-[600px] text-center">
+        <h1 className="landing-title">Website under construction!</h1>
+        <p className="landing-text">
+          Please come back another time. Thank you!
+        </p>
+        <p className="landing-text">:-)</p>
+      </div>
+    </div>
+  );
+}
