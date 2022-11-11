db289fc4a8216b6e70eef1d4b36c550d4d4de889
jackdvs <j4ckdavis@gmail.com>
2022-11-11 03:34:36 +0000

    added pacman

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
-import Head from "next/head";
-import Image from "next/image";
+import { PacmanLoader } from "react-spinners";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex justify-center items-center flex-col gap-3 p-4 min-h-[300px] sm:min-h-[600px] text-center">
-        <img src="/hack.jpeg" alt="hackintosh" />
-        {/* <h1 className="landing-title">Website under construction!</h1>
-        <p className="landing-text">
-          Please come back another time. Thank you!
-        </p>
-        <p className="landing-text">:-)</p> */}
+      <div className="flex justify-center items-center flex-col gap-4 p-4 min-h-[300px] sm:min-h-[600px] text-center">
+        <h1 className="landing-title">I am coda</h1>
+
+        <PacmanLoader
+          size={32}
+          className="-ml-[50px] float-left -mb-[50px]"
+          color="black"
+        />
+        <PacmanLoader size={32} className="-ml-[50px]" color="gold" />
       </div>
     </div>
   );
