8dbdeb4daaee1a63b78f9fa7db80898fe1f24c07
jackdvs <j4ckdavis@gmail.com>
2022-11-11 03:20:58 +0000

    added hackintosh logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <div className="flex justify-center items-center flex-col gap-3 p-4 min-h-[300px] sm:min-h-[600px] text-center">
-        <h1 className="landing-title">Website under construction!</h1>
+        <img src="/hack.jpeg" alt="hackintosh" />
+        {/* <h1 className="landing-title">Website under construction!</h1>
         <p className="landing-text">
           Please come back another time. Thank you!
         </p>
-        <p className="landing-text">:-)</p>
+        <p className="landing-text">:-)</p> */}
       </div>
     </div>
   );
